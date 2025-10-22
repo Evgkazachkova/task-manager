f81695f57518933fd4dd3be074f05d52bdeed89e
Evgenia Kazachkova <evgkazachkovaa@gmail.com>
2025-10-22 13:48:16 +0300

    feat: add header

--- task-manager-app/src/app/layout/header/header.component.ts
+++ task-manager-app/src/app/layout/header/header.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'tm-header',
   standalone: true,
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  createTask(): void {}
+}
